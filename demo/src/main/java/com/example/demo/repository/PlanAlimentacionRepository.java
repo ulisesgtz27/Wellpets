35c825eeaa3b9944694da3c0052b1e976943967a
ulisesgtz27 <159965725+ulisesgtz27@users.noreply.github.com>
2024-11-01 20:56:37 -0600

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/PlanAlimentacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.PlanAlimentacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanAlimentacionRepository extends JpaRepository<PlanAlimentacion, Long> {
-}
